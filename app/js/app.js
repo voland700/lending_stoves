dbf90aadcae9a67e439d6d854c609e1f15e1ea91
Max Volcov <voland700@yandex.ru>
2022-01-25 13:46:17 +0300

    finish products

--- app/js/app.js
+++ app/js/app.js
@@ -3,6 +3,16 @@
 
 document.addEventListener('DOMContentLoaded', () => {
 
-	// Custom JS
+function someHight(itemClass){
+  let item = document.querySelectorAll(itemClass);
+  let hightItem = 0;
+  for (let i = 0; i < item.length; i++) {
+    if (hightItem < item[i].offsetHeight) hightItem = item[i].offsetHeight;
+  }
+  for (let i = 0; i < item.length; i++) {
+    item[i].style.height = hightItem + 'px';
+  }
+}
+someHight('.product_summary');
 
 })
