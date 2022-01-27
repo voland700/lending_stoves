6f5814fd4528aae0dd88858348e6871decac6670
voland700 <voland700@yandex.ru>
2022-01-27 20:50:22 +0300

    finish index page

--- app/js/app.js
+++ app/js/app.js
@@ -3,16 +3,29 @@
 
 document.addEventListener('DOMContentLoaded', () => {
 
-function someHight(itemClass){
-  let item = document.querySelectorAll(itemClass);
-  let hightItem = 0;
-  for (let i = 0; i < item.length; i++) {
-    if (hightItem < item[i].offsetHeight) hightItem = item[i].offsetHeight;
-  }
-  for (let i = 0; i < item.length; i++) {
-    item[i].style.height = hightItem + 'px';
-  }
-}
-someHight('.product_summary');
+	let rellax = new Rellax('.rellax');
+	let paralax = new Rellax('.paralax', {
+    wrapper: '.advice'
+  });
+
+	function someHight(itemClass){
+		let item = document.querySelectorAll(itemClass);
+		let hightItem = 0;
+		for (let i = 0; i < item.length; i++) {
+			if (hightItem < item[i].offsetHeight) hightItem = item[i].offsetHeight;
+		}
+		for (let i = 0; i < item.length; i++) {
+			item[i].style.height = hightItem + 'px';
+		}
+	}
+	someHight('.product_summary');
+
+	if(document.getElementById('closeOnMap')){
+		document.getElementById('closeOnMap').addEventListener('click', function(event){
+			event.preventDefault();
+			document.querySelector('.map_info_wrap').style.display = 'none';
+		});
+	}
+
 
 })
